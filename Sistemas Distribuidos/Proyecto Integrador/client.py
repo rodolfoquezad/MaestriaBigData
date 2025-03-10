5cdca003a36f7dd9e556083b8ba979d04f47e346
rodolfoquezad <shelter.tj.devs@gmail.com>
2025-03-09 19:29:00 -0700

    Proyecto SD terminado

--- Sistemas Distribuidos/Proyecto Integrador/client.py
+++ Sistemas Distribuidos/Proyecto Integrador/client.py
@@ -1,7 +1,12 @@
 import socket
 import threading
 import tkinter as tk
-from tkinter import scrolledtext, simpledialog, messagebox
+from tkinter import scrolledtext, simpledialog, messagebox, filedialog
+import os
+from cryptography.fernet import Fernet
+
+KEY = b'T6-LALEyMf1tKdF40Iu48acmO23l4nR5toitrue6TUs='
+cipher_suite = Fernet(KEY)
 
 class Client:
     def __init__(self, root):
@@ -25,12 +30,18 @@ class Client:
         self.msg_entry.pack(fill=tk.X, pady=(0, 10))
         self.msg_entry.bind("<Return>", self.send_message)
         
+        self.send_file_button = tk.Button(self.chat_frame, text="Enviar Archivo", command=self.send_file)
+        self.send_file_button.pack(side=tk.LEFT, padx=(0, 5), pady=(0, 10))
+        
+        self.send_emoji_button = tk.Button(self.chat_frame, text="Enviar Emoji", command=self.show_emoji_menu)
+        self.send_emoji_button.pack(side=tk.LEFT, pady=(0, 10))
+        
         self.create_group_button = tk.Button(self.users_frame, text="Crear Grupo", command=self.create_group)
         self.create_group_button.pack(fill=tk.X, pady=(0, 10))
         
         self.add_user_button = tk.Button(self.users_frame, text="Agregar Usuario al Grupo", command=self.add_user_to_group)
         self.add_user_button.pack(fill=tk.X, pady=(0, 10))
-        self.add_user_button.config(state=tk.DISABLED)  # Inicialmente deshabilitado
+        self.add_user_button.config(state=tk.DISABLED)
         
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect(('127.0.0.1', 1001))
@@ -49,6 +60,29 @@ class Client:
         self.receive_thread = threading.Thread(target=self.receive_messages)
         self.receive_thread.start()
     
+    def send_message(self, event=None):
+        if not self.selected_user and not self.selected_group:
+            messagebox.showwarning("Selección", "Debes seleccionar un usuario o grupo para enviar un mensaje.")
+            return
+        message = self.msg_entry.get()
+        if message:
+            encrypted_message = cipher_suite.encrypt(message.encode('utf-8'))
+            print(f"Mensaje enviado: {encrypted_message}")
+            if self.selected_user:
+                self.client_socket.send(f"/private {self.selected_user} {encrypted_message.decode('utf-8')}".encode('utf-8'))
+                if self.selected_user not in self.chat_history:
+                    self.chat_history[self.selected_user] = []
+                self.chat_history[self.selected_user].append(f"Tú: {message}")
+                self.display_message(f"Tú: {message}")
+            elif self.selected_group:
+                self.client_socket.send(f"/groupmsg {self.selected_group} {encrypted_message.decode('utf-8')}".encode('utf-8'))
+                
+                if self.selected_group not in self.group_chat_history:
+                    self.group_chat_history[self.selected_group] = []
+                self.group_chat_history[self.selected_group].append(f"Tú: {message}")
+                self.display_message(f"Tú: {message}")
+            self.msg_entry.delete(0, tk.END)
+
     def receive_messages(self):
         while True:
             try:
@@ -63,47 +97,36 @@ class Client:
                     self.update_users_and_groups_list()
                 elif message.startswith("(Privado)"):
                     sender = message.split(":")[0].split()[1]
-                    if sender not in self.chat_history:
-                        self.chat_history[sender] = []
-                    self.chat_history[sender].append(message)
-                    
-                    if self.selected_user == sender:
-                        self.display_message(message)
+                    encrypted_message = message.split(":")[1].strip()
+                    print(f"Mensaje encriptado recibido: {encrypted_message}")
+                    try:
+                        decrypted_message = cipher_suite.decrypt(encrypted_message.encode('utf-8')).decode('utf-8')
+                        if sender not in self.chat_history:
+                            self.chat_history[sender] = []
+                        self.chat_history[sender].append(f"{sender}: {decrypted_message}")
+                        if self.selected_user == sender:
+                            self.display_message(f"{sender}: {decrypted_message}")
+                    except Exception as e:
+                        print(f"Error al desencriptar mensaje privado: {e}")
                 elif message.startswith("(Grupo"):
                     group_name = message.split("'")[1]
-                    if group_name not in self.group_chat_history:
-                        self.group_chat_history[group_name] = []
-                    self.group_chat_history[group_name].append(message)
-                    
-                    if self.selected_group == group_name:
-                        self.display_message(message)
-            except:
-                break
-
-    def send_message(self, event=None):
-        if not self.selected_user and not self.selected_group:
-            messagebox.showwarning("Selección", "Debes seleccionar un usuario o grupo para enviar un mensaje.")
-            return
-        
-        message = self.msg_entry.get()
-        if message:
-            if self.selected_user:
-                self.client_socket.send(f"/private {self.selected_user} {message}".encode('utf-8'))
-                
-                if self.selected_user not in self.chat_history:
-                    self.chat_history[self.selected_user] = []
-                self.chat_history[self.selected_user].append(f"{message}")
-                
-                self.display_message(f"{message}")
-            elif self.selected_group:
-                self.client_socket.send(f"/groupmsg {self.selected_group} {message}".encode('utf-8'))
-                
-                if self.selected_group not in self.group_chat_history:
-                    self.group_chat_history[self.selected_group] = []
-                self.group_chat_history[self.selected_group].append(f"{message}")
-                
-                self.display_message(f"{message}")
-            self.msg_entry.delete(0, tk.END)
+                    sender = message.split(":")[0].split()[1]
+                    encrypted_message = message.split(":")[1].strip()
+                    print(f"Mensaje encriptado recibido: {encrypted_message}")
+                    try:
+                        decrypted_message = cipher_suite.decrypt(encrypted_message.encode('utf-8')).decode('utf-8')
+                        if group_name not in self.group_chat_history:
+                            self.group_chat_history[group_name] = []
+                        self.group_chat_history[group_name].append(f"{sender}: {decrypted_message}")
+                        if self.selected_group == group_name:
+                            self.display_message(f"{sender}: {decrypted_message}")
+                    except Exception as e:
+                        print(f"Error al desencriptar mensaje de grupo: {e}")
+                elif message.startswith("FILE_RECEIVED:"):
+                    file_name = message.split(":")[1]
+                    self.display_message(f"Archivo recibido: {file_name}")
+            except Exception as e:
+                print(f"Error en receive_messages: {e}")
     
     def display_message(self, message):
         self.chat_area.config(state=tk.NORMAL)
@@ -161,10 +184,54 @@ class Client:
             else:
                 messagebox.showwarning("Error", f"El usuario '{user_to_add}' no está conectado.")
 
+    def send_file(self):
+        if not self.selected_user and not self.selected_group:
+            messagebox.showwarning("Selección", "Debes seleccionar un usuario o grupo para enviar un archivo.")
+            return
+        
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            file_name = os.path.basename(file_path)
+            
+            if self.selected_user:
+                self.client_socket.send(f"/privatefile {self.selected_user}".encode('utf-8'))
+            elif self.selected_group:
+                self.client_socket.send(f"/groupfile {self.selected_group}".encode('utf-8'))
+            
+            self.client_socket.send(file_name.encode('utf-8') + b'\n')
+            
+            with open(file_path, "rb") as file:
+                while True:
+                    file_data = file.read(4096)
+                    if not file_data:
+                        break
+                    self.client_socket.send(file_data)
+            
+            self.client_socket.send(b"<END_OF_FILE>")
+            
+            self.display_message(f"Archivo enviado: {file_name}")
+
+    def show_emoji_menu(self):
+        emoji_menu = tk.Menu(self.root, tearoff=0)
+        
+        emojis = ["😀", "😂", "😍", "🤔", "👍", "👋", "🎉", "❤️", "🔥", "🙏"]
+        
+        for emoji in emojis:
+            emoji_menu.add_command(label=emoji, command=lambda e=emoji: self.insert_emoji(e))
+        
+        emoji_menu.post(
+            self.send_emoji_button.winfo_rootx(),
+            self.send_emoji_button.winfo_rooty() + self.send_emoji_button.winfo_height()
+        )
+
+    def insert_emoji(self, emoji):
+        self.msg_entry.insert(tk.END, emoji)
+
 def main():
     root = tk.Tk()
-    client = Client(root)
+    Client(root)
     root.mainloop()
 
 if __name__ == '__main__':
     main()
+    
--- Sistemas Distribuidos/Proyecto Integrador/server.py
+++ Sistemas Distribuidos/Proyecto Integrador/server.py
@@ -1,11 +1,16 @@
 import socket
 import threading
+import os
+from cryptography.fernet import Fernet
+
+
+KEY = b'T6-LALEyMf1tKdF40Iu48acmO23l4nR5toitrue6TUs='
+cipher_suite = Fernet(KEY)
 
 class Server:
-
     def __init__(self):
-        self.clients = {} 
-        self.usernames = [] 
+        self.clients = {}
+        self.usernames = []
         self.groups = {}
 
     def start_server(self):
@@ -20,6 +25,7 @@ class Server:
             self.clients[username] = client_socket
             self.usernames.append(username)
             print(f"{username} se ha conectado desde {addr}")
+
             client_thread = threading.Thread(target=self.handle_client, args=(client_socket, username))
             client_thread.start()
 
@@ -28,20 +34,54 @@ class Server:
         self.send_user_groups(username)
         while True:
             try:
-                message = client_socket.recv(1024).decode('utf-8')
-                if message.startswith("/private"):
-                    _, recipient, private_message = message.split(' ', 2)
-                    self.send_private_message(username, recipient, private_message)
-                elif message.startswith("/creategroup"):
-                    _, group_name = message.split(' ', 1)
+                message_type = client_socket.recv(1024).decode('utf-8')
+                if not message_type:
+                    break
+                if message_type.startswith("/privatefile"):
+                    _, recipient = message_type.split(' ', 1)
+                    file_name = b""
+                    while True:
+                        chunk = client_socket.recv(1)
+                        if chunk == b'\n':
+                            break
+                        file_name += chunk
+                    try:
+                        file_name = file_name.decode('utf-8')
+                        self.receive_file(client_socket, username, recipient, file_name)
+                    except UnicodeDecodeError as e:
+                        print(f"[ERROR] No se pudo decodificar el nombre del archivo: {e}")
+                elif message_type.startswith("/groupfile"):
+                    _, group_name = message_type.split(' ', 1)
+                    file_name = b""
+                    while True:
+                        chunk = client_socket.recv(1)
+                        if chunk == b'\n':
+                            break
+                        file_name += chunk
+                    try:
+                        file_name = file_name.decode('utf-8')
+                        self.receive_group_file(client_socket, username, group_name, file_name)
+                    except UnicodeDecodeError as e:
+                        print(f"[ERROR] No se pudo decodificar el nombre del archivo: {e}")
+                elif message_type.startswith("/private"):
+                    parts = message_type.split(' ', 2)
+                    if len(parts) == 3:
+                        _, recipient, private_message = parts
+                        self.send_private_message(username, recipient, private_message)
+                    else:
+                        print(f"[ERROR] Invalid private message format: {message_type}")
+                elif message_type.startswith("/creategroup"):
+                    _, group_name = message_type.split(' ', 1)
                     self.create_group(group_name, username)
-                elif message.startswith("/addtogroup"):
-                    _, group_name, member = message.split(' ', 2)
+                elif message_type.startswith("/addtogroup"):
+                    _, group_name, member = message_type.split(' ', 2)
                     self.add_to_group(group_name, member, username)
-                elif message.startswith("/groupmsg"):
-                    _, group_name, group_message = message.split(' ', 2)
+                elif message_type.startswith("/groupmsg"):
+                    _, group_name, group_message = message_type.split(' ', 2)
                     self.send_group_message(username, group_name, group_message)
-            except:
+
+            except Exception as e:
+                print(f"Error: {e}")
                 break
 
         client_socket.close()
@@ -49,18 +89,44 @@ class Server:
         self.usernames.remove(username)
         self.broadcast_users_list()
 
-    def send_private_message(self, sender, recipient, message):
+    def send_private_message(self, sender, recipient, encrypted_message):
         if recipient in self.clients:
             try:
-                self.clients[recipient].send(f"{message}".encode('utf-8'))
+                self.clients[recipient].send(f"(Privado) {sender}: {encrypted_message}".encode('utf-8'))
             except:
                 self.clients[recipient].close()
+
+    def send_group_message(self, sender, group_name, encrypted_message):
+        if group_name in self.groups:
+            if sender in self.groups[group_name]:
+                for member in self.groups[group_name] and member != sender:
+                    try:
+                        self.clients[member].send(f"(Grupo '{group_name}') {sender}: {encrypted_message}".encode('utf-8'))
+                    except:
+                        self.clients[member].close()
+            else:
+                self.clients[sender].send(f"No eres miembro del grupo '{group_name}'.".encode('utf-8'))
+        else:
+            self.clients[sender].send(f"El grupo '{group_name}' no existe.".encode('utf-8'))
+
+    def send_group_message(self, sender, group_name, encrypted_message):
+        if group_name in self.groups:
+            if sender in self.groups[group_name]:
+                for member in self.groups[group_name]:
+                    try:
+                        self.clients[member].send(f"(Grupo '{group_name}') {sender}: {encrypted_message}".encode('utf-8'))
+                    except:
+                        self.clients[member].close()
+            else:
+                self.clients[sender].send(f"No eres miembro del grupo '{group_name}'.".encode('utf-8'))
+        else:
+            self.clients[sender].send(f"El grupo '{group_name}' no existe.".encode('utf-8'))
 
     def create_group(self, group_name, creator):
         if group_name not in self.groups:
             self.groups[group_name] = [creator]
             self.clients[creator].send(f"Grupo '{group_name}' creado con éxito.".encode('utf-8'))
-            self.send_user_groups(creator) 
+            self.send_user_groups(creator)
         else:
             self.clients[creator].send(f"El grupo '{group_name}' ya existe.".encode('utf-8'))
 
@@ -81,14 +147,64 @@ class Server:
         else:
             self.clients[requester].send(f"El grupo '{group_name}' no existe.".encode('utf-8'))
 
-    def send_group_message(self, sender, group_name, message):
+    def receive_file(self, client_socket, sender, recipient, file_name):
+        if recipient in self.clients:
+            try:
+                save_path = f"C:\\Archivos_Recibidos\\{recipient}"
+                os.makedirs(save_path, exist_ok=True)
+                
+                file_path = os.path.join(save_path, file_name)
+                with open(file_path, "wb") as file:
+                    while True:
+                        file_data = client_socket.recv(4096)
+                        if not file_data:
+                            break
+                        if file_data.endswith(b"<END_OF_FILE>"):
+                            file.write(file_data[:-len(b"<END_OF_FILE>")])
+                            break
+                        else:
+                            file.write(file_data)
+                self.clients[recipient].send(f"FILE_RECEIVED:{file_name}".encode('utf-8'))
+            except Exception as e:
+                print(f"[ERROR] Error al recibir archivo: {e}")
+        else:
+            self.clients[sender].send(f"El usuario '{recipient}' no está conectado.".encode('utf-8'))
+
+    def receive_group_file(self, client_socket, sender, group_name, file_name):
         if group_name in self.groups:
             if sender in self.groups[group_name]:
+                temp_file_path = f"C:\\Archivos_Recibidos\\{group_name}_{file_name}"
+                with open(temp_file_path, "wb") as temp_file:
+                    while True:
+                        file_data = client_socket.recv(4096)
+                        if not file_data:
+                            break
+                        
+                        if file_data.endswith(b"<END_OF_FILE>"):
+                            temp_file.write(file_data[:-len(b"<END_OF_FILE>")])
+                            break
+                        else:
+                            temp_file.write(file_data)
+                
                 for member in self.groups[group_name]:
-                    try:
-                        self.clients[member].send(f"{sender}: {message}".encode('utf-8'))
-                    except:
-                        self.clients[member].close()
+                    if member != sender:
+                        try:
+                            save_path = f"C:\\Archivos_Recibidos\\{member}"
+                            os.makedirs(save_path, exist_ok=True)
+                            
+                            file_path = os.path.join(save_path, file_name)
+                            with open(temp_file_path, "rb") as temp_file, open(file_path, "wb") as file:
+                                while True:
+                                    file_data = temp_file.read(4096)
+                                    if not file_data:
+                                        break
+                                    file.write(file_data)
+                            
+                            self.clients[member].send(f"FILE_RECEIVED:{file_name}".encode('utf-8'))
+                        except Exception as e:
+                            print(f"[ERROR] Error al recibir archivo para {member}: {e}")
+                
+                os.remove(temp_file_path)
             else:
                 self.clients[sender].send(f"No eres miembro del grupo '{group_name}'.".encode('utf-8'))
         else:
